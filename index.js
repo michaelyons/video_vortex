6e2dcd83961472028c7e7f22726269bb62b8f49b
Michael J. Lyons <mikelyons7@me.com>
2019-01-11 13:06:06 -0700

    Add recentDate method and update time on DOM

---
+++ index.js
@@ -0,0 +1,18 @@
+const recentDate = () => {
+  const date = new Date();
+  const values = {
+    hour: 'numeric',
+    minute: '2-digit',
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  };
+  const lastTime = date.toLocaleString('en-US', values);
+  const splitDate = lastTime.split(',');
+  const cleanDate = splitDate[3] + ' ' + splitDate[1] + ' ' + splitDate[2];
+  const lastUpdated = document.querySelector('#updated-time');
+  lastUpdated.innerHTML = 'last updated:' + cleanDate;
+};
+
+recentDate();
